dc558eac2094d3f14e12c5ba5c9f0492d524266d
Jovanne Sousa <jovanemaciel943@gmail.com>
2025-09-21 21:46:19 -0400

    fix: corrigindo url da api, to do: adicionar   headers: {    Authorization: Bearer ,  }

--- src/Store/reducers/auth.ts
+++ src/Store/reducers/auth.ts
@@ -5,7 +5,7 @@ import {
 } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-const API_URL = 'https://localhost:8080/api/auth'
+const API_URL = 'http://localhost:8080/auth'
 
 interface LoginResponse {
   username: string
